052f6ae71de0978dfe2448d73eed87afe7b047eb
Rookko_ <anthony.veillet@telus.net>
2024-03-05 17:06:50 -0500

    Ajout ofxbraitsh et histogramme

--- src/ui/histogramme.cpp
+++ src/ui/histogramme.cpp
@@ -1,2 +1,19 @@
 // ArtiFrame/ui/histogramme.cpp
 // Classe responsable de l'histogramme
+
+#include "ofxDatGui.h"
+
+class HistogramComponent : public ofxDatGuiFolder
+{
+public:
+
+    int redHist[256] = { 0 };
+    int greenHist[256] = { 0 };
+    int blueHist[256] = { 0 };
+
+    HistogramComponent(string label, ofColor color) : ofxDatGuiFolder(label, color) {}
+    void draw();
+    void setRedHist(int redHist[]);
+    void setGreenHist(int greenHist[]);
+    void setBlueHist(int blueHist[]);
+};
