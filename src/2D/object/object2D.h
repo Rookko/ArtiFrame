e364f3ad7ad406a1a4c38ea7f7df9805729ad55c
vicou80 <102197152+vicou80@users.noreply.github.com>
2024-03-06 16:35:36 -0500

    Update object2D.h

--- src/2D/object/object2D.h
+++ src/2D/object/object2D.h
@@ -9,16 +9,6 @@ class object2D
 public:
 	string name;
 	ofPoint coordinates;
-	int offsetX1 = 0;
-	int offsetY1 = 0;
-	int offsetX2 = 0;
-	int offsetY2 = 0;
-
-	int active = -1;
-
-	void setup();
-
-	void draw();
 
 };
 
