eb792d49ed424e067f288931bac0df3ac3dcab68
vicou80 <102197152+vicou80@users.noreply.github.com>
2024-03-06 21:42:46 -0500

    Revert "Ajout classe primitive2D et shape"
    This reverts commit d8eb6f7fddecbb0b1a117973ac4106e945576157.

--- primitive2D.cpp
+++
@@ -1,4 +0,0 @@
-// ArtiFrame/2D/primitive2D.cpp
-// Classe responsable des Primitive 2D de l'application 2D.
-
-#include "primitive2D.h"
--- shape.cpp
+++
@@ -1,4 +0,0 @@
-// ArtiFrame/2d/object/shape.cpp
-// Classe principale des formes 2D.
-
-#include "shape.h"
