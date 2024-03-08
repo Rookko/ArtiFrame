cf79e0d1f0839815d78c285700c93d0867fb899e
Mathieu Chretien <MACHR47@ulaval.ca>
2024-03-07 23:58:13 -0500

    cubePrimitive

---
+++ cubePrimitive.h
@@ -0,0 +1,20 @@
+#include "ofMain.h"
+#include "object.h"
+
+#pragma once
+
+class CubePrimitive : public Object {
+public:
+
+	ofBoxPrimitive* cube;
+
+	void drawWireframeOverride();
+
+	void drawSolidOverride();
+
+	void drawShaderOverride();
+
+	void drawBoundingBoxOverride();
+
+	ofVec3f getMeshBoundingBoxDimension(ofMesh* mesh);
+};
